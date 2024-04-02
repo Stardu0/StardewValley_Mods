0f2186860ba2d25a8ebe0353bd8e888b273e7dd8
Jihyeun Ryu <water14lily@naver.com>
2024-04-02 11:44:54 +0900

    feat: found player position

--- AutoKegManagement/AutoKegManagement/ModEntry.cs
+++ AutoKegManagement/AutoKegManagement/ModEntry.cs
@@ -14,8 +14,9 @@ namespace AutoKegManagement
     /// <summary>The mod entry point.</summary>
     internal sealed class ModEntry : Mod
     {
-        bool checkHealth = true;
-        bool checkSpeedBuff = true;
+        int[] dy = new int[8]{-1,0,1,-1,1,-1,0,1};
+        int[] dx = new int[8]{-1,-1,-1,0,0,1,1,1};
+
         /*********
         ** Public methods
         *********/
@@ -23,9 +24,7 @@ namespace AutoKegManagement
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
             helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
-            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
             helper.Events.GameLoop.OneSecondUpdateTicked += this.OnOneSecondUpdateTicked;
 
         }
@@ -37,77 +36,29 @@ namespace AutoKegManagement
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            //this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-        }
 
         private void OnUpdateTicked(object sender, EventArgs e)
         {
-            if (Game1.player.health > Game1.player.maxHealth * 0.3) checkHealth = true;
-
-            if (Game1.player.health < Game1.player.maxHealth * 0.3 && checkHealth)
-            {
-                // set variables
-                const string Cheese_ID = "424";
-                Item cheese = new StardewValley.Object(Cheese_ID, 1, false, -1, 2);
-                StardewValley.Object cheeseObj = new StardewValley.Object(Cheese_ID,1,false,-1,2);
-                // find cheese 
-                int idx = Game1.player.getIndexOfInventoryItem(cheese);
-
-                if(idx >= 0)
-                {
-                    checkHealth = false;
-                    Game1.player.eatObject(cheeseObj);
-                    Game1.player.Items.ReduceId(Cheese_ID, 1);
-                }
-            } 
-        }
-
-        private void OnDayStarted(object sender, EventArgs e)
-        {
-            // drink Triple Shot Espresso when the player wakes up
-            const string TSE_ID = "253";
-            Item TSE = new StardewValley.Object(TSE_ID, 1);
-            StardewValley.Object TSEObj = new StardewValley.Object(TSE_ID, 1);
-            Game1.player.eatObject(TSEObj);
-            Game1.player.Items.ReduceId(TSE_ID, 1);
 
         }
+
         private void OnOneSecondUpdateTicked(object sender, EventArgs e)
         {
+            // this.Monitor.Log($"Grab: {Game1.player.TilePoint}", LogLevel.Info);
+            Vector2 playerPos = Game1.player.TilePoint.ToVector2();
 
-            // update checkSpeedBuff
-            if (Game1.player.hasBuff("drink"))
+            for (int i = 0; i < 8; i++)
             {
-                if (Game1.player.buffs.AppliedBuffs["drink"].millisecondsDuration < 2000) checkSpeedBuff = true;
-            }
+                Vector2 newPlayerPos = playerPos;
+                newPlayerPos.X += dx[i];
+                newPlayerPos.Y += dy[i];
 
-            // check buff id
-            if (!Game1.player.hasBuff("drink") && checkSpeedBuff)
-            {
-                // set variables TrippleShotEspresso = TSE
-                const string TSE_ID = "253";
-                Item TSE = new StardewValley.Object(TSE_ID, 1);
-                StardewValley.Object TSEObj = new StardewValley.Object(TSE_ID, 1);
-
-                // check buff has speed buff
-                if (Game1.player.buffs.Speed == 0 && Game1.player.getIndexOfInventoryItem(TSE) >= 0)
+                if(StardewValley.Utility.canGrabSomethingFromHere((int)newPlayerPos.X, (int)newPlayerPos.Y, Game1.player))
                 {
-                    checkSpeedBuff = false;
-                    Game1.player.eatObject(TSEObj);
-                    Game1.player.Items.ReduceId(TSE_ID, 1);
+                    this.Monitor.Log($"X:{newPlayerPos.X} Y:{newPlayerPos.Y} Can Grab");
+
                 }
             }
-
-            //this.Monitor.Log($"BuffIDs: {Game1.player.buffs.AppliedBuffIds}", LogLevel.Info);
-            //this.Monitor.Log($"BuffApplied: {Game1.player.buffs.Speed}", LogLevel.Info);
-            //this.Monitor.Log($"BuffMovement: {Game1.player.getMovementSpeed()}", LogLevel.Info);
         }
 
     }
